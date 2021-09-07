bba66763a8430d44ce7aa6fbd8c64d24bad11409
Roman <joeres.roman@web.de>
2021-09-07 11:24:14 +0200

    Variable input and output directories implemented

--- workflow/scripts/create_scripts_bsite.py
+++ workflow/scripts/create_scripts_bsite.py
@@ -2,13 +2,13 @@ import os
 
 script = """
 import psico.fullinit
-load resources/structures/{protein}.pdb
+load {resources}/structures/{protein}.pdb
 from glob import glob
-lst = glob("resources/templates/*.pdb")
+lst = glob("{resources}/templates/*.pdb")
 for i in lst:cmd.load(i)
 extra_fit name CA, {protein}, tmalign
 select template, br. {protein} within {radius} of organic
-save results/parsed_structures_bsite/{protein}.pdb, template
+save {results}/parsed_structures_bsite/{protein}.pdb, template
 """
 
 outputs = snakemake.output
@@ -17,7 +17,9 @@ for output in outputs:
     with open(output, "w") as file:
         file.write(
             script.format(
+                resources=snakemake.config["source"],
                 protein=protein,
                 radius=snakemake.config["bsite"]["radius"],
+                results=snakemake.config["result"]
             )
         )
