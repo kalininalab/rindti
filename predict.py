0c483c7699cd6a0ad75f1cf2cc2bd71ea3603c83
Roman Joeres <Roman.Joeres@helmholtz-hzi.de>
2021-09-30 11:30:49 +0200

    Some more implementations on predictions

--- predict.py
+++ predict.py
@@ -24,26 +24,44 @@ def predict(**kwargs):
     val = Dataset(kwargs["data"], split="val", name=kwargs["name"])
     test = Dataset(kwargs["data"], split="test", name=kwargs["name"])
 
-    for dataset in [train, val, test]:
-        for index in len(dataset):
+    train_dataloader = DataLoader(train)
+    val_loader = DataLoader(val)
+    test_loader = DataLoader(test)
+
+    trainer = Trainer(
+        gpus=kwargs["gpu"],
+        deterministic=True,
+    )
+
+    train_result = trainer.test(model=model, dataloaders=train_loader)
+    val_result = trainer.test(model=model, dataloaders=val_loader)
+    test_result = trainer.test(model=model, dataloaders=test_loader)
+
+    exit(0)
+    for dataset in [test, train, val]:
+        print(len(dataset))
+        for index in range(len(dataset)):
             data = dataset.get(index)
             print(data)
+            print(data.y)
+            data = dataset.get(len(dataset) - index - 1)
+            print(data)
+            print(data.y)
             exit(0)
 
 
 if __name__ == '__main__':
-    tmp_parser = argparse.ArgumentParser()
-    tmp_parser.add_argument("-m", "--model", type=str, default="classification")
-    args = tmp_parser.parse_known_args()[0]
-    model_type = args.model
-
     parser = argparse.ArgumentParser()
+    parser.add_argument("-m", "--model", type=str, default="classification")
+    parser.add_argument("data", type=str)
     parser.add_argument("-c", "--checkpoint", type=str, required=True)
     parser.add_argument("-n", "--name", type=str, default=None, help="Subdirectory to store the graphs in")
     parser.add_argument("-d", "--debug", action='store_true', default=False, help="Flag to turn on the debug mode")
     parser.add_argument("-l", "--lectin")
     parser.add_argument("-g", "--glycan")
-    parser = models[model_type].add_arguments(parser)
-    argvars = parser.parse_args()
+    parser.add_argument("-s", "--seed", type=int, default=42)
+    parser.add_argument("-t", "--transformer", type=str, default="none")
+    # parser = models[model_type].add_arguments(parser)
+    argvars = vars(parser.parse_args())
 
     predict(**argvars)
