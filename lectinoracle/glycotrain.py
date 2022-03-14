e3cb53ff00344579b5918db996a4823620fa375d
Roman Joeres <Roman.Joeres@helmholtz-hzi.de>
2022-03-14 10:03:59 +0100

    Implementing of LectinOracle training

--- lectinoracle/glycotrain.py
+++ lectinoracle/glycotrain.py
@@ -1,14 +1,35 @@
 import pickle
-from glycowork.ml.models import prep_model
+
+import pandas as pd
+from glycowork.glycowork import lib
+from glycowork.ml.models import prep_model, LectinOracle
+from glycowork.ml.model_training import train_model as tm_lib, SAM
 from glycowork.ml.inference import get_multi_pred
 import torch
 import os
 import argparse
 import time
+import torch.nn as nn
+import torch.optim as optim
+from glycowork.motif.graph import glycan_to_graph
+from glycowork.motif.processing import get_lib
+
+from torch_geometric.data import Data
+from torch_geometric.loader import DataLoader
 
 from lectinoracle import extract
+from model_training_rwrkd import train_model as tm_own
+
+from rindti.models import ClassificationModel, RegressionModel
+from rindti.utils.data import TwoGraphData
+
+models = {
+    "classification": ClassificationModel,
+    "regression": RegressionModel,
+}
 
 cut = 1022
+lib += ["Gal3S", "Neu5Ac", "Gal4S", "Glc6S", "GlcNAc6S"]
 
 
 def run_esm_1b():
@@ -33,7 +54,7 @@ def extract_data(**kwargs):
 
     data = pickle.load(open(kwargs["data"], "rb"))
     lectin_data = data["prots"]
-    esm_embeds = pickle.load(open(kwargs["prot"], "rb")) if os.path.exists(kwargs["prot"]) else {}
+    esm_embeds = pickle.load(open(kwargs["scnd"], "rb")) if os.path.exists(kwargs["scnd"]) else {}
 
     tmp = []
     output = open("lectinoracle/seqs.fasta", "w")
@@ -50,47 +71,170 @@ def extract_data(**kwargs):
     if len(tmp) > 0:
         representations = run_esm_1b()
         esm_embeds = {**esm_embeds, **representations}
-        pickle.dump(esm_embeds, open(kwargs["prot"], "wb"))
+        pickle.dump(esm_embeds, open(kwargs["scnd"], "wb"))
     print(f"\tFinished in {time.time() - start:0.5f} sec")
 
 
-def inference(kwargs):
-    global cut
+def read_data(pickle_data, split, **kwargs):
+    file = open(pickle_data, "rb")
+    all_data = pickle.load(file)
+    lectin_data = all_data["prots"]
+    glycan_data = all_data["drugs"]
+    esm_embeds = pickle.load(open(kwargs["scnd"], "rb")) if os.path.exists(kwargs["scnd"]) else {}
 
+    data = []
+    for sample in all_data["data"]:
+        if sample["split"] != split:
+            continue
+        seq = lectin_data.loc[sample["prot_id"]].seqs[:cut]
+        lec = esm_embeds[seq]
+        gly = glycan_data.loc[sample["drug_id"]].IUPAC
+        gly = gly.replace("NeuAc", "Neu5Ac")
+        data.append((lec, gly, sample["label"]))
+
+    df = pd.DataFrame(data, columns=['seq', 'glycan', 'match'])
+    x = list(
+        zip(df.seq.values.tolist(), [glycan_to_graph(k, libr=kwargs["gly_lib"]) for k in df.glycan.values.tolist()]))
+    y = df.match.values.tolist()
+    dataset = [Data(x=torch.tensor(k[0][1][0], dtype=torch.long), y=torch.tensor(k[1], dtype=torch.float),
+                    edge_index=torch.tensor([k[0][1][1][0], k[0][1][1][1]], dtype=torch.long)) for k in
+               list(zip(x, y))]
+    for k in range(len(dataset)):
+        dataset[k].train_idx = torch.tensor(x[k][0], dtype=torch.float)
+
+    return DataLoader(dataset, batch_size=128, shuffle=True)
+
+
+def inference_oracle(**kwargs):
     extract_data(**kwargs)
 
     data = pickle.load(open(kwargs["data"], "rb"))
     lectin_data = data["prots"]
     glycan_data = data["drugs"]
-    esm_embeds = pickle.load(open(kwargs["prot"], "rb")) if os.path.exists(kwargs["prot"]) else {}
+    esm_embeds = pickle.load(open(kwargs["scnd"], "rb")) if os.path.exists(kwargs["scnd"]) else {}
 
-    print("====== evaluate LectinOracle =======")
-    oracle = prep_model("LectinOracle", num_classes=1, trained=True)
+    if kwargs["saved"] == "":
+        oracle = prep_model("LectinOracle", num_classes=1, trained=True)
+    else:
+        oracle = LectinOracle(input_size_prot=1280, input_size_glyco=len(lib) + 1, hidden_size=128, num_classes=1)
+        oracle.load_state_dict(kwargs["saved"])
+        oracle = oracle.cuda()
 
+    missing, mse, mae, seen = 0, 0, 0, 0
     for i, sample in enumerate(data["data"]):
+        print(f"\r{i}/{len(data['data'])}", end="")
         seq = lectin_data.loc[sample["prot_id"]].seqs[:cut]
         lec = esm_embeds[seq]
         gly = glycan_data.loc[sample["drug_id"]].IUPAC
         gly = gly.replace("NeuAc", "Neu5Ac")
-        print(f"{sample['prot_id']} | {gly} ({sample['label']}): {get_multi_pred(seq, [gly], oracle, esm_embeds)}")
+        try:
+            pred = get_multi_pred(seq, [gly], oracle, esm_embeds)[0]
+            mse += (pred - sample["label"]) ** 2
+            mae += abs(pred - sample["label"])
+            seen += 1
+        except Exception:
+            missing += 1
+    print("\nMissing:", missing, "|", len(data["data"]))
+    print("MSE:", mse / seen)
+    print("MAE:", mae / seen)
 
 
-def training(kwargs):
-    pass
+def inference_rindti(**kwargs):
+    data = pickle.load(open(kwargs["data"], "rb"))
+    lectin_data = data["prots"]
+    glycan_data = data["drugs"]
+
+    for version in os.listdir(kwargs["scnd"]):
+        model = models[kwargs["model"]].load_from_checkpoint(os.path.join(kwargs["scnd"], version))
+
+        for i, sample in enumerate(data["data"]):
+            prot_id = sample["prot_id"]
+            drug_id = sample["drug_id"]
+            prot_data = lectin_data.loc[prot_id, "data"]
+            drug_data = glycan_data.loc[drug_id, "data"]
+            new_i = {
+                "prot_count": float(lectin_data.loc[prot_id, "count"]),
+                "drug_count": float(glycan_data.loc[drug_id, "count"]),
+                "prot_id": prot_id,
+                "drug_id": drug_id,
+                "label": sample["label"],
+            }
+            new_i.update({"prot_" + k: v for (k, v) in prot_data.items()})
+            new_i.update({"drug_" + k: v for (k, v) in drug_data.items()})
+            two_graph_data = TwoGraphData(**new_i)
+
+
+def training(**kwargs):
+    def init_weights(m):
+        if type(m) == nn.Linear:
+            torch.nn.init.xavier_uniform(m.weight)
+
+    extract_data(**kwargs)
+
+    all_data = pickle.load(open(kwargs["data"], "rb"))
+    glycan_data = all_data["drugs"]
+    glycans = set()
+    data_min, data_max = float("inf"), float("-inf")
+
+    for sample in all_data["data"]:
+        gly = glycan_data.loc[sample["drug_id"]].IUPAC
+        gly = gly.replace("NeuAc", "Neu5Ac")
+        glycans.add(gly)
+        data_min = min(data_min, sample["label"])
+        data_max = max(data_max, sample["label"])
+
+    gly_lib = get_lib(list(glycans)) + ['GlcNAcOS', 'GalOS', 'HexNAc']
+    kwargs["gly_lib"] = gly_lib
+
+    print("===== Fill dataloader & model ======")
+    start = time.time()
+    train_loader = read_data(kwargs["data"], "train", **kwargs)
+    val_loader = read_data(kwargs["data"], "val", **kwargs)
+
+    model = LectinOracle(input_size_prot=1280, input_size_glyco=len(lib) + 1, hidden_size=128,
+                         num_classes=1, data_min=data_min, data_max=data_max)
+    model.apply(init_weights)
+    model.cuda()
+
+    optimizer = SAM(model.parameters(), optim.Adam, lr=0.1)
+    # optimizer = optim.Adam(model.parameters(), lr=0.0005)
+    scheduler = optim.lr_scheduler.CosineAnnealingLR(optimizer, 80)
+    criterion = nn.MSELoss().cuda()
+    print(f"\tFinished in {time.time() - start:0.5f} sec")
+
+    print("========== Start training ==========")
+    start = time.time()
+    model = tm_own(model=model, dataloaders={"train": train_loader, "val": val_loader}, criterion=criterion,
+                   optimizer=optimizer, scheduler=scheduler, num_epochs=100, patience=20, mode="regression")
+    print(f"\tFinished in {time.time() - start:0.5f} sec")
+    print("========= Finished training ========")
+
+    # save model
+    torch.save(model.state_dict(), "./oracle.pth")
+
+    # test
+    test_loader = read_data(kwargs["data"], "train", **kwargs)
 
 
 def main(**kwargs):
     if kwargs["train"]:
         training(**kwargs)
     else:
-        inference(**kwargs)
+        if kwargs["lo"]:
+            inference_oracle(**kwargs)
+        else:
+            kwargs["model"] = "classification" if "classification" in kwargs["scnd"] else "regression"
+            inference_rindti(**kwargs)
 
 
 if __name__ == "__main__":
     parser = argparse.ArgumentParser()
     parser.add_argument("data", type=str, help="Path to pickled dataset")
-    parser.add_argument("prot", type=str, help="Path to pickled dictionary mapping AA-sequences to ESM-1b embeddings")
-    parser.add_argument("-t", "--train", type=bool, default=False, dest="train")
+    parser.add_argument("scnd", type=str, help="Path to pickled dictionary mapping AA-sequences to ESM-1b embeddings "
+                                               "or path to pretrained RINDTI model")
+    parser.add_argument("-s", "--saved", type=str, default="", dest="saved", help="Path to oracle model weights")
+    parser.add_argument("-t", "--train", action='store_true', default=False, dest="train")
+    parser.add_argument("--lo", action='store_true', default=False, dest="lo")
     args = parser.parse_args()
 
     main(**vars(args))
---
+++ lectinoracle/model_training_rwrkd.py
@@ -0,0 +1,180 @@
+import copy
+import math
+import time
+
+import numpy as np
+import torch
+from glycowork.ml.model_training import EarlyStopping, disable_running_stats, enable_running_stats
+from matplotlib import pyplot as plt
+from sklearn.metrics import accuracy_score, matthews_corrcoef, mean_squared_error, mean_absolute_error, \
+    label_ranking_average_precision_score, ndcg_score
+
+
+def sigmoid(x):
+       return 1 / (1 + math.exp(-x))
+
+
+def train_model(model, dataloaders, criterion, optimizer,
+                scheduler, num_epochs=25, patience=50,
+                mode='classification', mode2='multi'):
+    """trains a deep learning model on predicting glycan properties\n
+    | Arguments:
+    | :-
+    | model (PyTorch object): graph neural network (such as SweetNet) for analyzing glycans
+    | dataloaders (PyTorch object): dictionary of dataloader objects with keys 'train' and 'val'
+    | criterion (PyTorch object): PyTorch loss function
+    | optimizer (PyTorch object): PyTorch optimizer
+    | scheduler (PyTorch object): PyTorch learning rate decay
+    | num_epochs (int): number of epochs for training; default:25
+    | patience (int): number of epochs without improvement until early stop; default:50
+    | mode (string): 'classification', 'multilabel', or 'regression'; default:classification
+    | mode2 (string): further specifying classification into 'multi' or 'binary' classification;default:multi\n
+    | Returns:
+    | :-
+    | Returns the best model seen during training
+    """
+    since = time.time()
+    early_stopping = EarlyStopping(patience=patience, verbose=True)
+    best_model_wts = copy.deepcopy(model.state_dict())
+    best_loss = 100.0
+    epoch_mcc = 0
+    if mode != 'regression':
+        best_acc = 0.0
+    else:
+        best_acc = 100.0
+    val_losses = []
+    val_acc = []
+
+    for epoch in range(num_epochs):
+        print('Epoch {}/{}'.format(epoch, num_epochs - 1))
+        print('-' * 10)
+
+        for phase in ['train', 'val']:
+            if phase == 'train':
+                model.train()
+            else:
+                model.eval()
+
+            running_loss = []
+            running_acc = []
+            running_mcc = []
+            for data in dataloaders[phase]:
+                try:
+                    x, y, edge_index, prot, batch = data.x, data.y, data.edge_index, data.train_idx, data.batch
+                    prot = prot.view(max(batch) + 1, -1).cuda()
+                except:
+                    x, y, edge_index, batch = data.x, data.y, data.edge_index, data.batch
+                x = x.cuda()
+                y = y.cuda()
+                edge_index = edge_index.cuda()
+                batch = batch.cuda()
+                optimizer.zero_grad()
+
+                with torch.set_grad_enabled(phase == 'train'):
+                    enable_running_stats(model)
+                    try:
+                        pred = model(prot, x, edge_index, batch)
+                        loss = criterion(pred, y.view(-1, 1))
+                    except:
+                        pred = model(x, edge_index, batch)
+                        loss = criterion(pred, y)
+
+                    if phase == 'train':
+                        loss.backward()
+                        optimizer.first_step(zero_grad=True)
+                        disable_running_stats(model)
+                        try:
+                            criterion(model(prot, x, edge_index, batch), y.view(-1, 1)).backward()
+                        except:
+                            criterion(model(x, edge_index, batch), y).backward()
+                        optimizer.second_step(zero_grad=True)
+
+                running_loss.append(loss.item())
+                if mode == 'classification':
+                    if mode2 == 'multi':
+                        pred2 = np.argmax(pred.cpu().detach().numpy(), axis=1)
+                    else:
+                        pred2 = [sigmoid(x) for x in pred.cpu().detach().numpy()]
+                        pred2 = [np.round(x) for x in pred2]
+                    running_acc.append(accuracy_score(
+                        y.cpu().detach().numpy().astype(int), pred2))
+                    running_mcc.append(matthews_corrcoef(y.detach().cpu().numpy(), pred2))
+                elif mode == 'multilabel':
+                    running_acc.append(label_ranking_average_precision_score(y.cpu().detach().numpy().astype(int),
+                                                                             pred.cpu().detach().numpy()))
+                    running_mcc.append(ndcg_score(y.cpu().detach().numpy().astype(int),
+                                                  pred.cpu().detach().numpy()))
+                else:
+                    running_acc.append(mean_squared_error(y.cpu().detach().numpy(), pred.cpu().detach().numpy()))
+
+            epoch_loss = np.mean(running_loss)
+            epoch_acc = np.mean(running_acc)
+            if mode != 'regression':
+                epoch_mcc = np.mean(running_mcc)
+            else:
+                epoch_mcc = 0
+            if mode == 'classification':
+                print('{} Loss: {:.4f} Accuracy: {:.4f} MCC: {:.4f}'.format(
+                    phase, epoch_loss, epoch_acc, epoch_mcc))
+            elif mode == 'multilabel':
+                print('{} Loss: {:.4f} LRAP: {:.4f} NDCG: {:.4f}'.format(
+                    phase, epoch_loss, epoch_acc, epoch_mcc))
+            else:
+                print('{} Loss: {:.4f} MSE: {:.4f}'.format(
+                    phase, epoch_loss, epoch_acc))
+
+            if phase == 'val' and epoch_loss <= best_loss:
+                best_loss = epoch_loss
+                best_model_wts = copy.deepcopy(model.state_dict())
+            if mode != 'regression':
+                if phase == 'val' and epoch_acc > best_acc:
+                    best_acc = epoch_acc
+            else:
+                if phase == 'val' and epoch_acc < best_acc:
+                    best_acc = epoch_acc
+            if phase == 'val':
+                val_losses.append(epoch_loss)
+                val_acc.append(epoch_acc)
+                early_stopping(epoch_loss, model)
+                try:
+                    scheduler.step(epoch_loss)
+                except:
+                    scheduler.step()
+
+        if early_stopping.early_stop:
+            print("Early stopping")
+            break
+        print()
+
+    time_elapsed = time.time() - since
+    print('Training complete in {:.0f}m {:.0f}s'.format(
+        time_elapsed // 60, time_elapsed % 60))
+    if mode == 'classification':
+        print('Best val loss: {:4f}, best Accuracy score: {:.4f}'.format(best_loss, best_acc))
+    elif mode == 'multilabel':
+        print('Best val loss: {:4f}, best LRAP score: {:.4f}'.format(best_loss, best_acc))
+    else:
+        print('Best val loss: {:4f}, best MSE score: {:.4f}'.format(best_loss, best_acc))
+    model.load_state_dict(best_model_wts)
+
+    ## plot loss & score over the course of training
+    fig, ax = plt.subplots(nrows=2, ncols=1)
+    plt.subplot(2, 1, 1)
+    plt.plot(range(epoch + 1), val_losses)
+    plt.title('Model Training')
+    plt.ylabel('Validation Loss')
+    plt.legend(['Validation Loss'], loc='best')
+
+    plt.subplot(2, 1, 2)
+    plt.plot(range(epoch + 1), val_acc)
+    plt.xlabel('Number of Epochs')
+    if mode == 'classification':
+        plt.ylabel('Validation Accuracy')
+        plt.legend(['Validation Accuracy'], loc='best')
+    elif mode == 'multilabel':
+        plt.ylabel('Validation LRAP')
+        plt.legend(['Validation LRAP'], loc='best')
+    else:
+        plt.ylabel('Validation MSE')
+        plt.legend(['Validation MSE'], loc='best')
+    return model
